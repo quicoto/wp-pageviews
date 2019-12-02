a81ac04dbb77c17ad9aec717ee1f73231a12452e
Ricard Torres <ricard.torres@netcentric.biz>
2019-12-02 10:42:25 +0100

    Fix fetch

--- main.js
+++ main.js
@@ -9,19 +9,21 @@
   if ($el) {
     fetch(wp_pageviews_ajax.ajax_url, {
       method: "POST",
-      cache: "no-cache",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/x-www-form-urlencoded"
       },
       credentials: 'same-origin',
       body: JSON.stringify(data),
     })
-    .then((response) => {
-      console.log('[WP Pageviews Plugin]');
-      console.log('Pageview recorded');
+    .then((resp) => resp.json())
+    .then(function(data) {
+      if(data.status == "success"){
+        console.log('[WP Pageviews Plugin]');
+        console.log('Pageview recorded');
 
-      if (response) {
-        $el.innerText = response;
+        if (response) {
+          $el.innerText = response;
+        }
       }
     })
     .catch((error) => {
